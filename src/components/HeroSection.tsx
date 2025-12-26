86d72b275a5c383c2c791345e4f829f7476de445
rajatbisht01 <rajat.bisht@solutionbriz.com>
2025-12-26 15:32:38 +0530

    fix_hero_image

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -25,7 +25,7 @@ export const HeroSection = () => {
       <AnimatePresence mode="wait">
         <motion.div
           key={currentImageIndex}
-          initial={{ opacity: 0, scale: 1.1 }}
+          initial={{ opacity: 0, scale: 1 }}
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.8, ease: "easeInOut" }}
