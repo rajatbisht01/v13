57d9027b2d6769a43873901774ad4940b11901f4
rajatbisht01 <rajat.bisht@solutionbriz.com>
2026-01-26 17:55:49 +0530

    fix_alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -150,10 +150,10 @@ export const Header = () => {
           </nav>
 
           {/* Desktop CTA */}
-          <div className="hidden lg:flex gap-4">
+          <div className="hidden lg:flex  lg:items-center  gap-4">
             <div ref={searchRef} className="relative">
               <button onClick={() => setIsSearchOpen(!isSearchOpen)}>
-                <SearchIcon className="w-5 h-5" />
+                <SearchIcon className="w-5 h-5 lg:mr-4" />
               </button>
               {isSearchOpen && (
                 <div className="absolute right-0 mt-2 w-96 bg-card rounded-xl shadow-xl">
